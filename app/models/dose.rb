d40f223d97d5a453d4f38b633289c6862ed8873e
Sandra Chatonnet <sandra.chatonnet@yahoo.fr>
2020-02-13 16:16:27 +0100

    html et controller ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,6 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :ingredient_id, :cocktail_id, presence: true
-  validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,13 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "milk")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "limonade")
+Cocktail.create(name: "Mojito")
+Dose.create(description: "1ml", ingredient_id: 1, cocktail_id: 1)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
